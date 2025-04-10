b52be5f9ff628b6b0cbe37c671d78613b64e54ce
mifalia <mifarivonjisoa@gmail.com>
2025-04-10 11:25:29 +0300

    feat: add session-based route guard control

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,15 @@
 import { Routes } from '@angular/router';
 import { LoginPage } from './pages/login/login.component';
 import { SignupComponent } from './pages/signup/signup.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { AuthGuard } from '../services/guards/auth.guard';
 
 export const routes: Routes = [
+  {
+    path: '',
+    component: DashboardComponent,
+    canActivate: [AuthGuard],
+  },
   {
     path: 'login',
     component: LoginPage,
