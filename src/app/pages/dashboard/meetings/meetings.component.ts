8d5a7f77ebab8f8a539b9d5a5a26950567f96c63
mifalia <mifarivonjisoa@gmail.com>
2025-04-19 19:42:41 +0300

    feat(meeting): add meeting tabs

--- src/app/pages/dashboard/meetings/meetings.component.ts
+++ src/app/pages/dashboard/meetings/meetings.component.ts
@@ -1,9 +1,40 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { ContactService } from '../../../services/firebase/firestore/contacts.service';
+import { generateUserAvatar } from '../../../shared/utils/user-utils';
 
 @Component({
   selector: 'app-meetings',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './meetings.component.html',
   styleUrl: './meetings.component.css',
 })
-export class MeetingsComponent {}
+export class MeetingsComponent implements OnInit {
+  constructor(private contactService: ContactService) {}
+
+  activeTab: 'join' | 'create' = 'join';
+
+  switchTab(tabId: 'join' | 'create'): void {
+    this.activeTab = tabId;
+  }
+
+  isActive(tabId: 'join' | 'create'): boolean {
+    return this.activeTab === tabId;
+  }
+
+  contacts: any = [];
+
+  async loadContacts() {
+    try {
+      this.contacts = await this.contactService.getContacts();
+    } catch (error) {
+      console.error('Error loading contacts:', error);
+    }
+  }
+
+  generateUserAvatar = (k: string | null) => generateUserAvatar(k as string);
+
+  ngOnInit(): void {
+    this.loadContacts();
+  }
+}
